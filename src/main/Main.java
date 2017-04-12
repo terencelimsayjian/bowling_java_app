888aa7ba6e824db69810097248fa607a061d577e
terence lim <terencelim92@gmail.com>
2017-04-12 13:00:45 +0800

    added random, average, and good bowlers

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Random Bowler");
         randomBowler();
+
+        System.out.println("Average Bowler");
+        averageBowler();
+
+        System.out.println("Good Bowler");
+        goodBowler();
     }
 
     private static void randomBowler() {
@@ -12,6 +18,42 @@ public class Main {
         while (game.isGamePlaying()) {
 
             int firstRoll = (int) (Math.random() * 10 + 1);
+            game.roll(firstRoll);
+
+            if (firstRoll != 10) {
+                int secondRoll = (int) (Math.random() * (10 - firstRoll) + 1);
+                game.roll(secondRoll);
+            }
+
+        }
+
+        game.score();
+    }
+
+    private static void averageBowler() {
+        Game game = new Game();
+
+        while (game.isGamePlaying()) {
+
+            int firstRoll = (int) (Math.random() * 5 + 6);
+            game.roll(firstRoll);
+
+            if (firstRoll != 10) {
+                int secondRoll = (int) (Math.random() * (10 - firstRoll) + 1);
+                game.roll(secondRoll);
+            }
+
+        }
+
+        game.score();
+    }
+
+    private static void goodBowler() {
+        Game game = new Game();
+
+        while (game.isGamePlaying()) {
+
+            int firstRoll = (int) (Math.random() * 3 + 8);
             game.roll(firstRoll);
 
             if (firstRoll != 10) {
